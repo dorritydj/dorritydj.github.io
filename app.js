ecd7ba6bd07a120173f4426aa5b9334cf91372d0
Daniel Dorrity <dorritydj@gmail.com>
2016-10-03 14:19:49 -0400

    Adding animation on the navigation

--- app.js
+++ app.js
@@ -9,6 +9,17 @@
 changeOpac("header", 1, 2500);
 
 /**
+ * Applies fade for navigation
+ */
+$('nav li a').hover(
+    function() {
+        $(this).stop().animate({backgroundColor:'#000'}, 200);
+    }, function () {
+        $(this).stop().animate({backgroundColor:'#212121'}, 100);
+    }
+);
+
+/**
  Fades in name after a button is pressed
  */
 
@@ -21,13 +32,20 @@ var curr = "";
 function switchInfo(which) {
     curr = which.id;
 
+    $("nav li a").removeClass("active")
+    $(which).addClass("active");
+
+    // TODO: Add functionality to set an active class on the selected nav item
+
     if( prev == "" ){
         changeOpac(curr, 1, 500);
         changeOpac("header", 0, 500);
+
         prev = curr;
     }else if( prev != ""){
         changeOpac(prev, 0, 500);
         changeOpac(curr, 1, 500);
+
         prev = curr
     }else if( prev == curr ){
         return;
