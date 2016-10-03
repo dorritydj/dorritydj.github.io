3a6fc02b8826323742e7f79a4e7980651d3fa355
Daniel Dorrity <dorritydj@gmail.com>
2016-10-03 00:44:06 -0400

    adding code to handle transitions

---
+++ app.js
@@ -0,0 +1,39 @@
+/**
+ * Created by dorritydj on 10/3/16.
+ */
+
+
+/**
+ Applies opening fade on page load
+ */
+changeOpac("header", 1, 2500);
+
+/**
+ Fades in name after a button is pressed
+ */
+
+/**
+ Changes what info in displayed in the middle depending on the button pressed
+ */
+var prev = "";
+var curr = "";
+
+function switchInfo(which) {
+    curr = which.id;
+
+    if( prev == "" ){
+        changeOpac(curr, 1, 500);
+        changeOpac("header", 0, 500);
+        prev = curr;
+    }else if( prev != ""){
+        changeOpac(prev, 0, 500);
+        changeOpac(curr, 1, 500);
+        prev = curr
+    }else if( prev == curr ){
+        return;
+    }
+}
+
+function changeOpac(div, i, time) {
+    $(div).animate({opacity: i}, time, "linear");
+}
